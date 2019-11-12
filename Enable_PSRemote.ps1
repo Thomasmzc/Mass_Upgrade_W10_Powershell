a771422cd4e648f241ab7c81ee8c49d47f631480
Thomasmzc <56715707+Thomasmzc@users.noreply.github.com>
2019-11-12 09:19:04 +0100

    Delete Enable_PSRemote.ps1

--- Enable_PSRemote.ps1
+++
@@ -1,9 +0,0 @@
-﻿$PSExec = "C:\Users\TMAZECOL\OneDrive - COMPUTACENTER\Documents\PowerShell_Installation_Windows10\bac_a_sable\psexec.exe"
-$server_names = Get-Content "C:\Users\TMAZECOL\OneDrive - COMPUTACENTER\Documents\PowerShell_Installation_Windows10\bac_a_sable\computers.txt"
-$user = "TMAZECOL"  
-$pass = "Clem291119"
-$command ="enable-psremoting -force"
- Foreach ($server in $server_names){
- Start-Process -Filepath "$PSExec" -ArgumentList "\\$server -u $user -p $pass -h -d powershell.exe $command"
- }
-
